e1379f7209de02aeae55a4d52b3b7257908e212e
DK Kim <dkim693@wisc.edu>
2023-12-09 19:37:49 -0600

    added Styles

--- components/Collatz_Sequence.js
+++ components/Collatz_Sequence.js
@@ -1,64 +1,82 @@
 "use client";
 import { useState } from "react";
+import styles from "../styles/collatz_sequence.css";
 
 function CollatzSequence() {
-  const [inputNumber, setInputNumber] = useState("");
-  const [sequence, setSequence] = useState([]);
+    const [inputNumber, setInputNumber] = useState("");
+    const [sequence, setSequence] = useState([]);
 
-  const collatzSequence = (n) => {
-    const sequenceArray = [];
+    const collatzSequence = (n) => {
+        const sequenceArray = [];
 
-    while (n !== 1) {
-      sequenceArray.push(n);
+        while (n !== 1) {
+            sequenceArray.push(n);
 
-      if (n % 2 === 0) {
-        n = n / 2;
-      } else {
-        n = 3 * n + 1;
-      }
-    }
-    sequenceArray.push(1);
+            if (n % 2 === 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+        }
+        sequenceArray.push(1);
 
-    setSequence(sequenceArray);
-  };
+        setSequence(sequenceArray);
+    };
 
-  const handleButtonClick = () => {
-    const number = parseInt(inputNumber);
+    const handleButtonClick = () => {
+        const number = parseInt(inputNumber);
 
-    if (!isNaN(number) && number > 0) {
-      collatzSequence(number);
-    } else {
-      alert("Please enter a positive natural number.");
-    }
-  };
+        if (!isNaN(number) && number > 0) {
+            collatzSequence(number);
+        } else {
+            alert("Please enter a positive natural number.");
+        }
+    };
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter a natural number"
-        value={inputNumber}
-        onChange={(e) => setInputNumber(e.target.value)}
-      />
-      <button onClick={handleButtonClick}>Generate Sequence</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Step</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sequence.map((number, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{number}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="main">
+            <div>
+                <div className={styles.description}>
+                    <h1>Collatz Sequence</h1>
+                    <p>
+                        The Collatz conjecture is a conjecture in mathematics that concerns a
+                        sequence defined as follows: start with any positive integer n. Then
+                        each term is obtained from the previous term as follows: if the
+                        previous term is even, the next term is one half of the previous
+                        term. If the previous term is odd, the next term is 3 times the
+                        previous term plus 1. The conjecture is that no matter what value of
+                        n, the sequence will always reach 1.
+                    </p>
+                </div>
+                <div>
+                    <input
+                        className="input-field"
+                        type="text"
+                        placeholder="Enter a natural number"
+                        value={inputNumber}
+                        onChange={(e) => setInputNumber(e.target.value)}
+                    />
+                    <button className="custom-button" onClick={handleButtonClick}>Generate Sequence</button>
+                    <table className="result-table">
+                        <thead>
+                            <tr>
+                                <th>Step</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sequence.map((number, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{number}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CollatzSequence;
